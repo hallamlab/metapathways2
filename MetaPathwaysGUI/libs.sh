c6952db79deb8ed8e3370ca79ce0cdbcffce495a
Niels Hanson <nielshanson@me.com>
2014-09-03 21:52:35 -0700

    updated with Unix MP GUI executable

---
+++ MetaPathwaysGUI/libs.sh
@@ -0,0 +1,13 @@
+cp /usr/lib/x86_64-linux-gnu/libXext.so.6 .
+cp /usr/lib/x86_64-linux-gnu/libX11.so.6 .
+cp /lib/x86_64-linux-gnu/libdl.so.2 .
+cp /lib/x86_64-linux-gnu/librt.so.1 .
+cp /lib/x86_64-linux-gnu/libpthread.so.0 .
+cp /usr/lib/x86_64-linux-gnu/libstdc++.so.6 .
+cp /lib/x86_64-linux-gnu/libm.so.6 .
+cp /lib/x86_64-linux-gnu/libgcc_s.so.1 .
+cp /lib/x86_64-linux-gnu/libc.so.6 .
+cp /lib64/ld-linux-x86-64.so.2 .
+cp /usr/lib/x86_64-linux-gnu/libxcb.so.1 .
+cp /usr/lib/x86_64-linux-gnu/libXau.so.6 .
+cp /usr/lib/x86_64-linux-gnu/libXdmcp.so.6 .
